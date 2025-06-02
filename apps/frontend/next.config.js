ce9d9638dedeb2382a381b71ff76a6acdf7a864a
Stanislav Lanskoy <wisekaa03@gmail.com>
2025-06-02 19:11:22 +0300

    frontend

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -9,7 +9,8 @@ const { composePlugins, withNx } = require('@nx/next');
 const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
-  nx: {},
+  nx: {
+  },
 };
 
 const plugins = [
